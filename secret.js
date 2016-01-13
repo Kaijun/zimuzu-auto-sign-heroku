dd6d745feb6614d804a6816a6c099cd5f445096d
Kaijun <Demonbane.cn@gmail.com>
2016-01-13 13:14:52 +0100

    multiple account supported

--- secret.js
+++ secret.js
@@ -1,6 +1,26 @@
-module.exports = {
-  account: "请输入你的帐号",
-  password: "请输入你的密码",
-  remember: 1,
-  url_back: 'http://www.zimuzu.tv/user/user/index'
-}
+module.exports = [
+  {
+    account: "请输入你的帐号",
+    password: "请输入你的密码",
+    remember: 1,
+    url_back: 'http://www.zimuzu.tv/user/user/index'
+  }
+]
+
+/* 要是有多个帐号密码*/
+/*
+module.exports = [
+  {
+    account: "请输入第一个的帐号",
+    password: "请输入第一个的密码",
+    remember: 1,
+    url_back: 'http://www.zimuzu.tv/user/user/index'
+  },
+  {
+    account: "请输入第二个的帐号",
+    password: "请输入第二个的密码",
+    remember: 1,
+    url_back: 'http://www.zimuzu.tv/user/user/index'
+  }
+]
+*/
