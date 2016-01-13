68eecd8828869679a20bbe5a97bc179e664283da
Kaijun <Demonbane.cn@gmail.com>
2016-01-13 13:12:49 +0100

    no message

--- secret.js
+++ secret.js
@@ -1,6 +1,14 @@
-module.exports = {
-  account: "Demonbane",
-  password: "114051306",
-  remember: 1,
-  url_back: 'http://www.zimuzu.tv/user/user/index'
-}
+module.exports = [
+  {
+    account: "Demonbane",
+    password: "114051306",
+    remember: 1,
+    url_back: 'http://www.zimuzu.tv/user/user/index'
+  },
+  {
+    account: "chenkj918",
+    password: "114051306",
+    remember: 1,
+    url_back: 'http://www.zimuzu.tv/user/user/index'
+  }
+]
