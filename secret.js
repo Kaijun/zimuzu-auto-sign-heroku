9429a32fa8b3666f8477ec306f7426222af93c3e
Kaijun <Demonbane.cn@gmail.com>
2016-01-15 03:05:39 +0100

    no message

--- secret.js
+++ secret.js
@@ -1,26 +1,14 @@
 module.exports = [
   {
-    account: "请输入你的帐号",
-    password: "请输入你的密码",
-    remember: 1,
-    url_back: 'http://www.zimuzu.tv/user/user/index'
-  }
-]
-
-/* 要是有多个帐号密码*/
-/*
-module.exports = [
-  {
-    account: "请输入第一个的帐号",
-    password: "请输入第一个的密码",
+    account: "Demonbane",
+    password: "114051306",
     remember: 1,
     url_back: 'http://www.zimuzu.tv/user/user/index'
   },
   {
-    account: "请输入第二个的帐号",
-    password: "请输入第二个的密码",
+    account: "chenkj918",
+    password: "114051306",
     remember: 1,
     url_back: 'http://www.zimuzu.tv/user/user/index'
-  }
+  },
 ]
-*/
